f00672ec912ae72155e7191e533b50bc2a3c8c14
ansonantony <ansonantony89@gmail.com>
2024-04-21 14:34:47 +0530

    frontend and backend integration with axios and jwt token retrieval

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/court', {
+mongoose.connect('mongodb://127.0.0.1:27017/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
